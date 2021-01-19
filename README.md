c78c2999a7b41cd7b2b00ddfd5e7eae5f9074c23
Luke Pritchard <luke-pritch@users.noreply.github.com>
2021-01-19 14:36:59 -0500

    repo init

---
+++ README.md
@@ -0,0 +1,3 @@
+# AlgoExpert Solutions
+
+This is my repo where I write out my solutions to algoexpert.io problems. Written in TypeScript.
---
+++ solutions/arrays/twoSum.ts
@@ -0,0 +1,23 @@
+// looks like a hash table question
+
+export function twoNumberSum(array: number[], targetSum: number) {
+  // Write your code here
+  for (let i = 0; i < array.length - 1; i++) {
+    const firstElement = array[i]
+    for (let j = i + 1; j < array.length; j++) {
+      const secondElement = array[j]
+      const sum = firstElement + secondElement
+      if (sum === targetSum) {
+        return [firstElement, secondElement]
+        console.log('found sum')
+      }
+    }
+  }
+  console.log('returned outside the loop')
+  return []
+}
+
+const array1 = [3, 5, -4, 8, 11, 1, -1, 6]
+const target = 10
+
+twoNumberSum(array1, target)
